2a2cde89b657b4923abb2d5927711ba8912a7603
elchapoguzman <pbae.sehc@gmail.com>
2019-05-16 18:10:32 -0400

    commit2

--- App1/App1/pages/login.xaml.cs
+++ App1/App1/pages/login.xaml.cs
@@ -12,9 +12,28 @@ namespace App1.pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class login : ContentPage
 	{
-		public login ()
-		{
-			InitializeComponent ();
-		}
-	}
+        public login()
+        {
+            InitializeComponent();
+            loginButton.Clicked += LoginButton_Clicked;
+        }
+
+        async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            string nombre = userEntry.Text;
+            string pass = passEntry.Text;
+            if (string.IsNullOrEmpty(nombre))
+            {
+                await DisplayAlert("Error", "Ingrese usuario", "Aceptar");
+                userEntry.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(pass))
+            {
+                await DisplayAlert("Error", "Debe ingresar Password", "Aceptar");
+                passEntry.Focus();
+                return;
+            }
+        }
+    }
 }

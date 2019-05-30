7645ca5a4ef04b9207c85b2eb354e558dc5c88ee
elchapoguzman <pbae.sehc@gmail.com>
2019-05-30 19:53:03 -0400

    commit api

--- App1/App1/pages/login.xaml.cs
+++ App1/App1/pages/login.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,6 +33,35 @@ namespace App1.pages
             {
                 await DisplayAlert("Error", "Debe ingresar Password", "Aceptar");
                 passEntry.Focus();
+                return;
+            }
+            //activo la espera
+            waitActivityIndicator.IsRunning = true;
+            //bloqueo el boton
+            loginButton.IsEnabled = false;
+            HttpClient client = new HttpClient();
+            //direccion base raiz
+            client.BaseAddress = new Uri("http://service.twk.cl");
+
+            // método asincrono await espera la respuesta
+            string jsonData = "{'nombre' :'" + nombre + "', 'pass' :'" + pass + "' }";
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("/api/alumno/201033292", content);
+
+            //leer la respuesta como strin y devuelva el resultado
+            string result = await response.Content.ReadAsStringAsync();
+
+            loginButton.IsEnabled = true;
+            waitActivityIndicator.IsRunning = true;
+
+            //se valida el resultado 
+            if (string.IsNullOrEmpty(result) || result == "null")
+            {
+                await DisplayAlert("Error", "Usuario o Clave erronea", "Aceptar");
+                userEntry.Text = string.Empty;
+                passEntry.Text = string.Empty;
+                userEntry.Focus();
                 return;
             }
         }
